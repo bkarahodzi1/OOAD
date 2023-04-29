ed052f4a88c4e97e28e71b3d64ae13237a34f97e
aprasovic1 <aprasovic1@etf.unsa.ba>
2023-04-29 14:30:04 +0200

    Dodane klase, atributi i get set

--- BrainBoost/Models/Question.cs
+++ BrainBoost/Models/Question.cs
@@ -8,7 +8,7 @@ namespace BrainBoost.Models
         public string Id { get; set; }
         public string QuestionText { get; set; }
         public string Description { get; set; }
-        public List<QuestionAnswer> Answers { get; set; }
+        public List<QuestionAnswer>? Answers { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     
