04add056c0714dfb0ebc4a869891791dc598c19b
Asmir <Asmir@HP-LAPTOP>
2023-04-29 13:22:11 +0200

    Add project files.

---
+++ BrainBoost/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BrainBoost.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
