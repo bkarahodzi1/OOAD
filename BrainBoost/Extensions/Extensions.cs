bbb2d2e15f7ec57a24a706038455a25e2316f99e
Imran <imranspahic300@gmail.com>
2023-05-31 22:27:59 +0200

    dodani extensions za active class u navigaciji

---
+++ BrainBoost/Extensions/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+
+namespace BrainBoost.Extensions
+{
+    public static class MvcExtensions
+    {
+        public static string ActiveClass(this IHtmlHelper htmlHelper, string controllers = null, string actions = null, string cssClass = "active")
+        {
+            var currentController = htmlHelper?.ViewContext.RouteData.Values["controller"] as string;
+            var currentAction = htmlHelper?.ViewContext.RouteData.Values["action"] as string;
+
+            var acceptedControllers = (controllers ?? currentController ?? "").Split(',');
+            var acceptedActions = (actions ?? currentAction ?? "").Split(',');
+
+            return acceptedControllers.Contains(currentController) && acceptedActions.Contains(currentAction)
+                ? cssClass
+                : "";
+        }
+    }
+}
+
---
+++ BrainBoost/Views/Shared/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+
+public static class MvcExtensions
+{
+    public static string ActiveClass(this IHtmlHelper htmlHelper, string controllers = null, string actions = null, string cssClass = "active")
+    {
+        var currentController = htmlHelper?.ViewContext.RouteData.Values["controller"] as string;
+        var currentAction = htmlHelper?.ViewContext.RouteData.Values["action"] as string;
+
+        var acceptedControllers = (controllers ?? currentController ?? "").Split(',');
+        var acceptedActions = (actions ?? currentAction ?? "").Split(',');
+
+        return acceptedControllers.Contains(currentController) && acceptedActions.Contains(currentAction)
+            ? cssClass
+            : "";
+    }
+}
