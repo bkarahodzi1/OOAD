c8f1150cf651cce8bebeed4faef24ea476421b56
Nedim Hosic <nhosic3@etf.unsa.ba>
2023-06-03 15:24:38 +0200

    zavrsen FeedbackController

--- BrainBoost/Controllers/FeedbackController.cs
+++ BrainBoost/Controllers/FeedbackController.cs
@@ -176,7 +176,6 @@ namespace BrainBoost.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-
         ///GET: Professor/FeedbackComment
         public IActionResult FeedbackComment()
         {
